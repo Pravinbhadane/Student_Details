d9a5990400a593cbd2fcfc5d8933bc7b82788b1a
Pravin Bhadane <pravinbhadane60@gmail.com>
2023-06-24 14:04:48 +0530

    Add project files.

---
+++ Student_Details/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Student_Details;
+
+class Program
+{
+    static void Main()
+    {
+      
+        Student student = new Student
+        {
+            RollNo = 15,
+            Name = "Pravin Bhadane",
+            Percentage = 85.5f
+        };
+
+        Console.WriteLine("Student Details:");
+        Console.WriteLine($"Roll No: {student.RollNo}");
+        Console.WriteLine($"Name: {student.Name}");
+        Console.WriteLine($"Percentage: {student.Percentage}%");
+    }
+}
---
+++ Student_Details/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Student_Details
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public float Percentage { get; set; }
+    }
+
+   
+}
